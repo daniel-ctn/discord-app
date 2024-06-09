84c0bf9a5556420353c67acb15eb7347c2bb854e
Daniel <danielnguyen5201@gmail.com>
2024-06-10 05:47:36 +0700

    set up theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Josefin_Sans } from 'next/font/google'
 import './globals.css'
 import { ClerkProvider } from '@clerk/nextjs'
+import { ThemeProvider } from '@/components/ThemeProvider'
 
 const inter = Josefin_Sans({ subsets: ['latin'] })
 
@@ -18,7 +19,11 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={inter.className}>{children}</body>
+        <body className={inter.className}>
+          <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+            {children}
+          </ThemeProvider>
+        </body>
       </html>
     </ClerkProvider>
   )
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
+import { UserButton } from '@clerk/nextjs'
+import { ModeToggle } from '@/components/ThemeToggle'
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Helu
+      <UserButton/>
+      <ModeToggle/>
     </main>
   )
 }
